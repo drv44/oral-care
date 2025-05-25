c52af5f49edffce467035d75e0f44f14e892588a
drv44 <dhruvsarkar960@gmail.com>
2025-05-25 22:47:12 +0530

    Updated frontend endpoint in main.py

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://oral-care.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
